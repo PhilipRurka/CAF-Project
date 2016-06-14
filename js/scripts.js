50c2a1568a0125a4c7a3c4af84bfa9694b09fb7b
Philip Rurka <philiprurka@gmail.com>
2016-06-14 15:31:43 -0400

    Update

--- js/scripts.js
+++ js/scripts.js
@@ -23,10 +23,24 @@
 
 
 
+
 /*****************
       Mouse
 *****************/
 
+$('.threeway button').click(function(event) {
+    event.preventDefault();
+    $('#overlay').addClass('overlay');
+    $('#pre-page').addClass('pre-page');
+    $('.menu').addClass('display');
+});
+
+$('.cross button').click(function(event) {
+    event.preventDefault();
+    $('#overlay').removeClass('overlay');
+    $('#pre-page').removeClass('pre-page');
+    $('.menu').removeClass('display');
+});
 
 
 
