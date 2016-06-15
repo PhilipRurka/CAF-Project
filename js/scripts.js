d531f80d18cb5993d94a34620e9619d8532aa7e1
Philip Rurka <philiprurka@gmail.com>
2016-06-14 22:36:07 -0400

    Update

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,8 @@
                          	Var
 ************************************************************/
 
-
+var file = "file:///Users/philiprurka/Documents/Dev/Sub-Projects/CAF/CAF-Project/pages/";
+var file_index = "file:///Users/philiprurka/Documents/Dev/Sub-Projects/CAF/CAF-Project/";
 
 
 
@@ -28,33 +29,110 @@
       Mouse
 *****************/
 
+
 /***** Menu *****/
 $('.threeway button').click(function(event) {
     event.preventDefault();
     $('#overlay').addClass('overlay');
     $('#pre-page').addClass('pre-page-menu');
-    $('.page').addClass('page-menu');
 
     setTimeout(function() {
-        $('.menu').addClass('display');
-        }, 500);
-        });
+        $('.menu').fadeIn(500);
+    }, 500);
+});
 
 $('.cross button').click(function(event) {
     event.preventDefault();
-    $('#overlay').removeClass('overlay');
-    $('#pre-page').removeClass('pre-page-menu');
-    $('.page').removeClass('page-menu');
-    $('.menu').removeClass('display');
+    $('.menu').fadeOut(500);
+
+    setTimeout(function() {
+        $('#overlay').removeClass('overlay');
+        $('#pre-page').removeClass('pre-page-menu');
+    }, 500);
 });
 
+
 /***** Topic *****/
-// $('. button').click(function(event) {
-//
-// });
+$('.topic a').click(function(event) {
+    event.preventDefault();
+    $('#overlay').addClass('overlay');
+    $('#pre-page').addClass('pre-page-topic');
+
+    setTimeout(function() {
+        $('.topic-list').fadeIn(500);
+    }, 500);
+});
+
+$('.cross button').click(function(event) {
+    event.preventDefault();
+    $('.topic-list').fadeOut(500);
+
+    setTimeout(function() {
+        $('#overlay').removeClass('overlay');
+        $('#pre-page').removeClass('pre-page-topic');
+    }, 500);
+});
 
 
+$('.topic-container .food').click(function(event) {
+    event.preventDefault();
+    $('.topic-list').fadeOut(500);
+    setTimeout(function () {
+        $('#overlay').removeClass('overlay');
+        $('#pre-page').removeClass('pre-page-topic');
+    }, 500);
+    setTimeout(function () {
+        window.location.href = file_index + "index.html";
+    },1000);
+    });
 
+$('.topic-container .drink').click(function(event) {
+    event.preventDefault();
+    $('.topic-list').fadeOut(500);
+    setTimeout(function () {
+        $('#overlay').removeClass('overlay');
+        $('#pre-page').removeClass('pre-page-topic');
+    }, 500);
+    setTimeout(function () {
+        window.location.href = file + "drink.html";
+    },1000);
+});
+
+$('.topic-container .treats').click(function(event) {
+    event.preventDefault();
+    $('.topic-list').fadeOut(500);
+    setTimeout(function () {
+        $('#overlay').removeClass('overlay');
+        $('#pre-page').removeClass('pre-page-topic');
+    }, 500);
+    setTimeout(function () {
+        window.location.href = file + "treat.html";
+    },1000);
+});
+
+$('.topic-container .tools').click(function(event) {
+    event.preventDefault();
+    $('.topic-list').fadeOut(500);
+    setTimeout(function () {
+        $('#overlay').removeClass('overlay');
+        $('#pre-page').removeClass('pre-page-topic');
+    }, 500);
+    setTimeout(function () {
+        window.location.href = file + "tool.html";
+    },1000);
+});
+
+$('.topic-container .room').click(function(event) {
+    event.preventDefault();
+    $('.topic-list').fadeOut(500);
+    setTimeout(function () {
+        $('#overlay').removeClass('overlay');
+        $('#pre-page').removeClass('pre-page-topic');
+    }, 500);
+    setTimeout(function () {
+        window.location.href = file + "room.html";
+    },1000);
+});
 
 /*****************
        Key
